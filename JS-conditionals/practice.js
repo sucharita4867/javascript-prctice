bbb6805bcf488c7470f8a27d74b615abd2150c53
sucharita4867 <sucharitasardar7@gmail.com>
2025-05-05 13:08:03 +0530

    task 3 practice is complete

--- JS-conditionals/practice.js
+++ JS-conditionals/practice.js
@@ -8,18 +8,31 @@
 //             console.log('odd numbers ;', num)
 //       }
 // }
-
+// ---------------------------------
 // task 2
+// {let num = -100;
+//       if(num > 0){
+//             console.log('positive no :' , num) ;
+//       }
+//       else if(num < 0 ){
+//             console.log('negative no :' , num);
+//       }
+//       else{
+//             console.log('zero :' , num);
+//       }
+// }
+// --------------------------------------------
+// task 3
+// {let num1 = 4 ;
+//  let num2 = 20 ;
+//       if (num1 > num2){
+//             console.log('big :' , num1)
+//       }  
+//       else if(num2 > num1){
+//             console.log('big :', num2)
+//       } 
+//       else{
+//             console.log('equal :', num1 )
+//       }
 
-{let num = -100;
-      if(num > 0){
-            console.log('positive no :' , num) ;
-      }
-      else if(num < 0 ){
-            console.log('negative no :' , num);
-      }
-      else{
-            console.log('zero :' , num);
-      }
-
-}
+// }
