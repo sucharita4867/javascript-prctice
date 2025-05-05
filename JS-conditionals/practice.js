0147807900c745807cebe36e6bbecf0ad08f3767
sucharita4867 <sucharitasardar7@gmail.com>
2025-05-05 12:55:46 +0530

    task 2 practice is complete

--- JS-conditionals/practice.js
+++ JS-conditionals/practice.js
@@ -1,9 +1,25 @@
 // chaking even and odd numbers
-{let num = 21;
-      if (num % 2 === 0) {
-            console.log('even number :' , num)
+// task 1
+// {let num = 21;
+//       if (num % 2 === 0) {
+//             console.log('even number :' , num)
+//       }
+//       else if (num % 2 !== 0){
+//             console.log('odd numbers ;', num)
+//       }
+// }
+
+// task 2
+
+{let num = -100;
+      if(num > 0){
+            console.log('positive no :' , num) ;
       }
-      else if (num % 2 !== 0){
-            console.log('odd numbers ;', num)
+      else if(num < 0 ){
+            console.log('negative no :' , num);
       }
+      else{
+            console.log('zero :' , num);
+      }
+
 }
