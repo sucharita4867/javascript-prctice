f2b7ac628c7997e129de872bd873741916c80dc0
sucharita4867 <sucharitasardar7@gmail.com>
2025-05-06 22:41:57 +0530

    practice javascript if else

--- JS-conditionals/practice-10-problems.js
+++ JS-conditionals/practice-10-problems.js
@@ -15,3 +15,36 @@
 //       console.log('negative :', number)
 // }
 // task 3
+// let num = 0 ;
+// if(num === 0){
+//       console.log('it is zero', num);
+// }
+// else {
+//       console.log('it is not zero', num)
+// }
+// task 4
+// let age = 1 ;
+// if (age >= 18){
+//       console.log('vote dite parbe', age)
+// }
+// else if(age < 18){
+//       console.log('vote dite parbe na', age)
+// }
+// task 5
+// let num1 = 37 ;
+// let num2 = 299 ;
+// if (num1 > num2){
+//       console.log('this number is big', num1)
+// }
+// else if(num2 > num1){
+//       console.log('this number is big', num2)
+// }
+// task 6
+// let num1 = 7 ;
+// let num2 = 7 ;
+// if(num1 === num2) {
+//       console.log('This number is equal')
+// }
+// else{
+//       console.log('This number is not equal')
+// }
