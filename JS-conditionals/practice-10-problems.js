93953266d07112d242c39bf6b519d4737e68389b
sucharita4867 <sucharitasardar7@gmail.com>
2025-05-06 22:24:26 +0530

    practice if else problems

---
+++ JS-conditionals/practice-10-problems.js
@@ -0,0 +1,17 @@
+// task 1
+// let number = 45 ;
+// if (number % 2 === 0){
+//       console.log('even :', number)
+// }
+// else {
+//       console.log('odd :', number)
+// }
+// task 2
+// let number = -10;
+// if (number > 0){
+//       console.log('positive :', number)
+// }
+// else if (number < 0){
+//       console.log('negative :', number)
+// }
+// task 3
