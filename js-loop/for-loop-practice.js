813d6fb94dcba04c156a20ac3fe77ab79dc47096
sucharita4867 <sucharitasardar7@gmail.com>
2025-05-13 22:55:36 +0530

    practice in decremental for loop

--- js-loop/for-loop-practice.js
+++ js-loop/for-loop-practice.js
@@ -1,8 +1,16 @@
-// for loop incremental practice
+// ---------------------for loop incremental practice---------------------
 // for(let s = 5 ; s <= 20 ; s++){
 //       console.log(s);
 // }
 // ====================
-for(let n = 0 ; n <= 10 ; n++){
-      console.log(n);
-}
+// for(let n = 0 ; n <= 20 ; n++){
+//       console.log(n);
+// }
+// ------------------------for loop decremental practice-----------------------
+// for (let s = 29 ; s >= 10 ; s--){
+//       console.log(s);
+// }
+// ===========================
+// for (let n = 15 ; n >= 0 ; n--){
+//       console.log(n);
+// }  
