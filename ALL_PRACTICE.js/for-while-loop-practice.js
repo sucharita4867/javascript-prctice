a0e61bc51512f0dbdecff426beef99b05380741a
sucharita4867 <sucharitasardar7@gmail.com>
2025-05-17 12:40:23 +0530

    for loop &  while loop incrememtal &decremental practice

--- ALL_PRACTICE.js/for-while-loop-practice.js
+++ ALL_PRACTICE.js/for-while-loop-practice.js
@@ -2,8 +2,58 @@
 // ------------incremental & decremental practice in for & while loop----------------
 // =============for
 {
-      // incremental
-      for (let s = 0 ; s <= 20 ; s++){
-            console.log('s');
+      // FOR Loop Incremental & Decremental Practice
+     {
+      //  INCREMENTAL
+      {
+      //       for(let n = 0 ; n <= 20 ; n++){
+      //       console.log(n);
+      // }
+      // // --------------------
+      // for (let b = 1 ; b <= 10 ; b++){
+      //       console.log(b);
+      // }
+      }
+      // DECREMENTAL
+      {
+      //       for (let i = 20 ; i >= 0 ; i--){
+      //       console.log(i);
+      // }
+      // // --------------------------
+      // for(let j = 50 ; j >= 20 ; j--){
+      //       console.log(j);
+      // }
+      }
+     }
+//      WHILE Loop Incremental & Decremental Practice
+      {
+            // INCREMENTAL
+         {
+            //   let i = 0 ;
+              // while(i <= 20){
+              //       console.log(i);
+              //       i++ ;
+              // }
+              // ---------------------
+              // let p = 0 ;
+              // while (p <= 10){
+              //       console.log(p) ;
+              //       p++ ;
+              // }
+         }
+      // DECREMENTAL
+         {
+            // let k = 10 ;
+            // while(k >= -10){
+            //       console.log(k);
+            //       k-- ;
+            // }
+            // -----------------------
+            // let h = 100 ;
+            // while(h >= -100){
+            //       console.log(h) ;
+            //       h-- ;
+            // }
+         }
       }
 }
