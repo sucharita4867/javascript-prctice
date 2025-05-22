309cda9eb9cc9a81985c5b6e1f02a9fb1dfe81eb
sucharita4867 <sucharitasardar7@gmail.com>
2025-05-22 18:12:01 +0530

    practice trim and case propaty

--- js-string-object/case.js
+++ js-string-object/case.js
@@ -1,25 +1,26 @@
-const school = 'Mohon Pur K. K. G. C. Institiution';
+{
+      // const school = 'Mohon Pur K. K. G. C. Institiution';
 // console.log(school);
 
 // console.log(school.toLowerCase());
 // console.log(school.toUpperCase());
 
 
-let subject = 'Geography';
-let book = 'geography';
+// let subject = 'Geography';
+// let book = 'geography';
 
 // subject = subject.toUpperCase()
 // book = book.toUpperCase()
 
 
-if(subject.toLowerCase() === book.toLowerCase()) {
-      console.log('exam hole purai fatai felbo');
-      console.log(subject)
-      console.log(book)
-}
-else{
-      console.log('exam hole bose bose ghumabo');
-}
+// if(subject.toLowerCase() === book.toLowerCase()) {
+      // console.log('exam hole purai fatai felbo');
+      // console.log(subject)
+      // console.log(book)
+// }
+// else{
+      // console.log('exam hole bose bose ghumabo');
+// }/
 
 
 // const drink = 'water ';
@@ -31,3 +32,39 @@ else{
 // else{
 //       console.log('na ami water khabo na');
 // }
+}
+
+{
+      {
+      //       const name = 'sucharita' ;
+      //       const nam = 'SuchAritA';
+      //       console.log(name);
+      //       console.log(nam);
+            
+      // console.log(name.toLowerCase())
+      // console.log(nam.toLowerCase())
+      //       if(name.toLowerCase() === nam.toLowerCase()){
+      //             console.log('name thik ache :', name)
+      //       }
+      //       else{
+      //             console.log('name vul ache :', nam)
+      //       }
+      }
+
+      {
+            let name = '  sucharita';
+            let num = ' sucharita  ';
+            console.log(name);
+            console.log(num); 
+      
+            console.log(name.trim());
+            console.log(num.trim());
+            
+            if(name.trim() === num.trim()){
+                  console.log('name thik ache')
+            }
+            else{
+                  console.log('name vul ache')
+            }
+      }
+}
