7442d7891dc77ba63cab6b19d61fed177757050f
sucharita4867 <sucharitasardar7@gmail.com>
2025-05-22 22:27:41 +0530

    concat and split practice

--- js-string-object/concat.js
+++ js-string-object/concat.js
@@ -1,5 +1,5 @@
-const first = 'sucha';
-const last = 'Rita';
+{// const first = 'sucha';
+// const last = 'Rita';
 
 // const fullname = first + last;
 // const fullName = first + ' ' + last;
@@ -11,3 +11,16 @@ const last = 'Rita';
 // --------------------
 // Includes
 // console.log(last.includes('r'));
+}
+
+let firstname = 'sucharita' ;
+let lastname = 'sardar' ;
+// const fullName = firstname + lastname ;
+// const fullName = firstname + ' ' + lastname ;
+// console.log(fullName)
+// ----------------------
+const fullName2 = firstname.concat(' ').concat(lastname);
+// console.log(fullName2) ;
+// --------------------
+// includes 
+console.log(lastname.includes('a'))
--- js-string-object/slice.js
+++ js-string-object/slice.js
@@ -6,12 +6,12 @@
 const sentence = 'I am a good and hardworking woman';
 // console.log(sentence.split(' '));
 // ----------------------------------------
-const friendsStr = 'anu,sucho,racho,farish,sabana,reba,sanjida,saniya';
+// const friendsStr = 'anu,sucho,racho,farish,sabana,reba,sanjida,saniya';
 // console.log(friendsStr.split(','))
-const friends = friendsStr.split(',');
+// const friends = friendsStr.split(',');
 // console.log(friends);
 // ------------------------------------------------
-const friendship = ['anu', 'sucho', 'racho', 'farish', 'sabana', 'reba', 'sanjida',];
+// const friendship = ['anu', 'sucho', 'racho', 'farish', 'sabana', 'reba', 'sanjida',];
 // console.log(friendship.join());
 // console.log(friendship.join('|'));
 // console.log(friendship.join('-'));
@@ -20,4 +20,18 @@ let collage = 'mograhut collage' ;
 // let part = collage.slice(5,8);
 // let part = collage.slice(0,5);
 let part = collage.slice(13,16);
-console.log(part);
+// console.log(part);
+// ---------------------------
+let myself = 'i am a good girl and i am vary hardworking';
+// console.log(myself.split(' '));
+// --------------------------------
+// const bondhuStr = 'moni , anu , reba , racho , sucho , sahil , hasu' ;
+// console.log(bondhuStr);
+// let bondhu = bondhuStr.split(',') ;
+// console.log(bondhu
+// ---------------------------------------
+const realfriend = ['moni' , 'anu', 'reba' , 'racho' , 'sucho' , 'sahil' , 'hasu'];
+// console.log(realbondhu);
+console.log(realfriend.join());
+console.log(realfriend.join('-'));
+console.log(realfriend.join('|'));
