19ad65ea9b5c2bc410c182f614daae1ac5c133b2
sucharita4867 <sucharitasardar7@gmail.com>
2025-05-22 22:51:32 +0530

    practice reverse join

--- js-string-object/reverse.js
+++ js-string-object/reverse.js
@@ -1,4 +1,5 @@
-const sentence = 'I am learning web dev.';
+{
+      const sentence = 'I am learning web dev.';
 // const result = '.ved bew gninrael ma I' ;
 
 // -------------------
@@ -13,5 +14,25 @@ for(const letter of sentence){
 // --------------------------------
 // rouls-2
 const reversed = sentence.split('').reverse().join('');
-console.log(reversed)
-
+// console.log(reversed)
+}
+// let reverse = ''
+const sentence = 'i am a developer im am hardworking' ;
+// for(const letter of sentence){
+//       // console.log(letter) ;
+//       reverse = letter + reverse ;
+// }
+// console.log(reverse)
+// ------------------------------
+let reverse = ''
+for (let i = 0 ; i < sentence.length ; i++){
+      // console.log(i);
+      // console.log(sentence[i]);
+      const letter = sentence[i];
+      reverse = letter + reverse ;
+}
+// console.log(reverse);
+// -------------------------------------
+// short cut
+let rev = sentence.split('').reverse().join('');
+console.log(rev)
