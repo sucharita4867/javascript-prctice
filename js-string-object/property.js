d9073d1e8ef9d491bfb84c9f646c0cae28365f6f
sucharita4867 <sucharitasardar7@gmail.com>
2025-05-23 07:21:38 +0530

    practice object

--- js-string-object/property.js
+++ js-string-object/property.js
@@ -1,11 +1,11 @@
-const person = {
-      name : 'Rabi',
-      age : 23 ,
-      profession : 'developer',
-      salary : 20000,
-      married : true,
-      'family member' : ['bou', 'chele', 'ma', 'baba'] 
-};
+// const person = {
+//       name : 'Rabi',
+//       age : 23 ,
+//       profession : 'developer',
+//       salary : 20000,
+//       married : true,
+//       'family member' : ['bou', 'chele', 'ma', 'baba'] 
+// };
 // console.log(person);
 
 // dot notation
@@ -16,11 +16,34 @@ const person = {
 // -----------------------------------------
 // bracket notation
 // third bracket diye access kora
-console.log(person['salary'])
-const income = person['salary'];
-console.log(income)
+// console.log(person['salary'])
+// const income = person['salary'];
+// console.log(income)
 
 
-console.log(person['family member'])
-const barirLok = person['family member'];
-console.log(barirLok)
+// console.log(person['family member'])
+// const barirLok = person['family member'];
+// console.log(barirLok)
+// -----------------------------------
+let women = {
+      nam : 'sahil',
+      boyos : 20 ,
+      profession : 'developer',
+      salary : 20000,
+      married : true,
+      'family member' : ['bou', 'chele', 'ma', 'baba'],
+}
+// console.log(women.profession)
+// const kikore = women.profession;
+// console.log(kikore)
+
+// const money = women.salary;
+// console.log(money)
+// =======================================
+// console.log(women['salary'])
+// const money = women['salary'];
+// console.log(money)
+
+console.log(women['family member'])
+const barirlok = women['family member'] ;
+console.log(barirlok);
