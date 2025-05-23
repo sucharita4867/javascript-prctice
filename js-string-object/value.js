988d625119adacd1d0bf5ba0239c8e1a82d5a92a
sucharita4867 <sucharitasardar7@gmail.com>
2025-05-23 07:30:59 +0530

    object change practice

--- js-string-object/value.js
+++ js-string-object/value.js
@@ -7,10 +7,19 @@ const person = {
       'family member' : ['bou', 'chele', 'ma', 'baba'] 
 };
 
-person.salary = 400000;
-person['name'] = 'sahil';
-person['family member'] =['chacha', 'chachu', 'mamu', 'khala']
+// person.salary = 400000;
+// person['name'] = 'sahil';
+// person['family member'] =['chacha', 'chachu', 'mamu', 'khala']
 
-const sahilMony = 'profession';
-person[sahilMony] = 'docter'
-console.log(person)
+// const sahilMony = 'profession';
+// person[sahilMony] = 'docter'
+// console.log(person)
+
+// ====================================
+person.name = 'sahil';
+person.age = 20 ;
+person.salary = 'none' ;
+person["family member"] = ['baba' ,'ma', 'vai', 'bon'] ;
+person.married = 'none' ;
+person.profession = 'student';
+console.log(person);
