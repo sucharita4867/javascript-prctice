7ecb6b5276ef46b74e27c0e198dfb479d390ce41
sucharita4867 <sucharitasardar7@gmail.com>
2025-05-24 11:49:09 +0530

    added for loop and while loop practice

--- js-array-more/looping.js
+++ js-array-more/looping.js
@@ -10,7 +10,8 @@ const friends = ['racho', 'sucho', 'anu', 'farish', 'sabana', 'moni'];
 for (const friend of friends) {
       // console.log(friend)
 }
-// ===========1
+{
+      // ===========1
 // for (let i = 0 ; i < friends.length ; i++){
 //       console.log(friends[i])
 // }
@@ -18,11 +19,14 @@ for (const friend of friends) {
 for (let n = 0 ; n < friends.length ; n++){
       // console.log(friends[n])
 }
+}
 
 //+++++++++++while loop diye practice++++++++++++++++
 
 const numbers = [12, 2324, 342, 5435, 3463, 42341, 334];
-let n = 0;
+// while loop practice
+{
+      let n = 0;
 while (n < numbers.length){
       // console.log(numbers[n]);
       n++;
@@ -30,5 +34,31 @@ while (n < numbers.length){
 
 // for loop
 for(let i = 0 ; i < numbers.length ; i++){
-      console.log(numbers[i])
+      // console.log(numbers[i])
+}
+}
+// for loop practice
+{
+      for(let k = 0 ; k < numbers.length; k++){
+      // console.log(numbers[k]);
+}
+// ------------------------------
+for(let o = 0 ; o < friends ; o++){
+      // console.log(friends[o]);
+}
+}
+// while loop practice
+{
+      const friendship = ['racho', 'sucho', 'anu', 'farish', 'sabana', 'moni'];
+let i = 0 ;
+while(i < friendship.length){
+      // console.log(friendship[i]);
+      i++ ;
+}
+const number1 = [12, 2324, 342, 5435, 3463, 42341, 334];
+let n = 0 ;
+while (n < number1.length){
+      console.log(number1[n]);
+      n++ ;
+}
 }
