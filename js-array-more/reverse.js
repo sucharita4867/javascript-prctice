348efabcf65dd38d68964c11a6452b4e6be85c5e
sucharita4867 <sucharitasardar7@gmail.com>
2025-05-24 15:30:54 +0530

    complete reverse practice

--- js-array-more/reverse.js
+++ js-array-more/reverse.js
@@ -1,24 +1,66 @@
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 // console.log(numbers);
 // --------1 proses-------
-// numbers.reverse();
+{
+      // numbers.reverse();
 // console.log(numbers)
-
+}
 // ----------2 proses---------
-const reverse = numbers.reverse();
-console.log(numbers);
+{
+      //  const reverse = numbers.reverse();
+      // console.log(numbers);
+}
 // ---------3 proses-----------
-// const revNumber = []; 
+{
+      // / const revNumber = []; 
 // for (const num of numbers){
 //       // console.log(num);
 //       revNumber.unshift(num);
 // }
 // console.log(revNumber)
+}
 // ==========1
-const friends = ['racho', 'sucho', 'anu', 'farish', 'sabana', 'moni'];
+{
+      // const friends = ['racho', 'sucho', 'anu', 'farish', 'sabana', 'moni'];
 // console.log(friends);
 // friends.reverse();
 // console.log(friends)
 // ===========2
-const reversed = friends.reverse();
+// const reversed = friends.reverse();
 // console.log(friends);
+}
+// ================================================================================
+const friendship = ['racho', 'sucho', 'anu', 'farish', 'sabana', 'moni'];
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// ------------------------------
+friendship.reverse();
+// console.log(friendship)
+// ---------------------------------+++++++++++++++++++++++++++++++++++++
+// const reverse = friendship.reverse();
+// console.log(reverse);
+// ----------------------------------------
+// const revFriend = [] ;
+// for (const friend of friendship){
+//       console.log(friend);
+//       revFriend.unshift(friend);
+// }
+// console.log(revFriend);
+// -----------------------------------------
+// const rev_number = [] ;
+// for (const num of numbers){
+//       console.log(num);
+//       rev_number.unshift(num)
+// }
+// console.log(rev_number);
+// -------------------------------------------
+const members = ['sucharita' , 'sucheta' , 'ankita' , 'susmita', 'momota' , 'anita'] ;
+
+// const rev_member = [];
+// for(const member of members){
+//       // console.log(member);
+//       rev_member.unshift(member);
+// }
+// // console.log(rev_member);
+// ----------------------------------------
+// members.reverse();
+// console.log(members);
