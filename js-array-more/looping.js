2c615c68f4c8497e9f134b8cd2c3b8fa98ce682e
sucharita4867 <sucharitasardar7@gmail.com>
2025-05-24 12:00:37 +0530

    this file practice is complete

--- js-array-more/looping.js
+++ js-array-more/looping.js
@@ -10,6 +10,7 @@ const friends = ['racho', 'sucho', 'anu', 'farish', 'sabana', 'moni'];
 for (const friend of friends) {
       // console.log(friend)
 }
+// for loop practice
 {
       // ===========1
 // for (let i = 0 ; i < friends.length ; i++){
@@ -18,6 +19,32 @@ for (const friend of friends) {
 // ===========2
 for (let n = 0 ; n < friends.length ; n++){
       // console.log(friends[n])
+}
+// -----------------------------------
+const numbers0 = [12, 2324, 342, 5435, 3463, 42341, 334];
+for (let k = 0 ; k < numbers0.length ; k++){
+            // console.log(numbers0[k]);
+}
+// ---------------------------------
+const friendgrang = ['racho', 'sucho', 'anu', 'farish', 'sabana', 'moni'];
+for (let p = 0 ; p < friendgrang.length ; p++){
+      // console.log(friendgrang[p]);
+}
+}
+// while loop practice
+{
+      let num = [34, 45, 89, 90, 1321, 3452, 1234, 53,];
+let o = 0 ;
+while(o < num.length){
+      // console.log(num[o]);
+      o++ ;
+}
+// ---------------------------------------------------
+let person = ['rabi', 'kobi', 'hobi', 'hasu', 'sahil', 'moni'];
+let u = 0 ;
+while (u < person.length){
+      console.log(person[u]);
+      u++ ;
 }
 }
 
@@ -58,7 +85,7 @@ while(i < friendship.length){
 const number1 = [12, 2324, 342, 5435, 3463, 42341, 334];
 let n = 0 ;
 while (n < number1.length){
-      console.log(number1[n]);
+      // console.log(number1[n]);
       n++ ;
 }
 }
