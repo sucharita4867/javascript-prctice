5b07855b1f37a56a6c6e2423cd382d4dbfb4da9a
sucharita4867 <sucharitasardar7@gmail.com>
2025-05-25 20:58:13 +0530

    square task practice

---
+++ js-function/parameter.js
@@ -0,0 +1,21 @@
+// --------------------borgo bar korar niyom ---------------------
+// 3----> 3 * 3
+// 5----> 5 * 5
+// 8----> 8 * 8
+// 12----> 12 * 12
+
+function square (number){
+      // console.log(number);
+      console.log('value of the number parameter :', number);
+      const borgo = number * number ;
+      console.log('square of the given number is :', borgo)
+}
+square(9);
+console.log('----------------------------------');
+square(12);
+console.log('----------------------------------');
+square(224);
+console.log('----------------------------------');
+square(5654);
+console.log('----------------------------------');
+square(893212);
