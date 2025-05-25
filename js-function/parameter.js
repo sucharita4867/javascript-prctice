ae9f31c8e7cf0778bc356c183910f47d3b5c5388
sucharita4867 <sucharitasardar7@gmail.com>
2025-05-25 21:08:36 +0530

    parameter practice is complete

--- js-function/parameter.js
+++ js-function/parameter.js
@@ -10,12 +10,22 @@ function square (number){
       const borgo = number * number ;
       console.log('square of the given number is :', borgo)
 }
-square(9);
+// square(9);
+// console.log('----------------------------------');
+// square(12);
+// console.log('----------------------------------');
+// square(224);
+// console.log('----------------------------------');
+// square(5654);
+// console.log('----------------------------------');
+// square(893212);
+
+// =================================================================
+function all (num1 , num2 , num3 , num4 , num5){
+      // console.log(all);
+      const sum = num1 + num2 + num3 + num4 + num5 ;
+      console.log(sum);
+}
+all(92 , 23, 94 ,36, 87);
 console.log('----------------------------------');
-square(12);
-console.log('----------------------------------');
-square(224);
-console.log('----------------------------------');
-square(5654);
-console.log('----------------------------------');
-square(893212);
+all(324,35345,567,242,79785,);
