eabe9a52c0e3235ecab4646db1fc8e5424392f4c
sucharita4867 <sucharitasardar7@gmail.com>
2025-05-25 17:14:23 +0530

    today practice function declear and cll

---
+++ js-function/function.js
@@ -0,0 +1,9 @@
+// -------------------function declare process ---------------------
+function lightOnKaro (){
+      console.log('age bose thkeke uthe darau');
+      console.log('bored er kache jau');
+      console.log('switch ta off thkeke on karo');
+      console.log('and chole aso');
+}
+// call the function
+lightOnKaro();
