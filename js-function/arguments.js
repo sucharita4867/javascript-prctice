2967f16234beb44e107539f1aafdd19ab18df6eb
sucharita4867 <sucharitasardar7@gmail.com>
2025-05-25 21:57:54 +0530

    difference practice is complete

---
+++ js-function/arguments.js
@@ -0,0 +1,37 @@
+function duble (num1){
+      const total = num1 * 2 ;
+      console.log(num1 , total) ;
+}
+// duble(34);
+// console.log('--------------------------');
+// duble(90);
+// console.log('----------------------------------');
+// duble(50);
+// console.log('----------------------------------');
+// duble(200);
+// // ----------------------------------------------
+// const taka = 400 ;
+// duble(taka);
+// // --------------------------------------------------
+// const money = 500 ;
+// duble(money);
+// ====================================================================================
+// difference in father and my
+{
+      function difference (num1 , num2){
+      const diff = num1 - num2 ;
+      console.log(num1 , num2 , 'difference is :', diff);
+}
+const fatherAge = 50 ;
+const myAge = 19 ;
+// difference(fatherAge , myAge);
+}
+
+// difference in mther and my
+function difference (numu , numS){
+      const diff = numu - numS ;
+      console.log(numu , numS , 'difference is :' ,diff);
+}
+const motherAge = 45 ;
+const myAge = 19 ;
+difference(motherAge , myAge);
