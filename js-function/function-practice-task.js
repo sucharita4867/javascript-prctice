8c494ca6eeddd43df6778e7beb28e9c92c01b2b4
sucharita4867 <sucharitasardar7@gmail.com>
2025-05-26 22:22:53 +0530

    practice task

---
+++ js-function/function-practice-task.js
@@ -0,0 +1,24 @@
+// task 1 (complete)
+{
+      function all(a,b,c,d){
+      // console.log(all);
+      const number = a * b * c * d;
+      // console.log(number);
+      }
+      // all(82,95,47,78)
+}
+// ----------------------------
+// task 2(incomplete)
+{
+      function numbers (num1 , num2){
+      if(num1 % 2 === 0){
+            return num1 / 2;
+      }
+      else{
+            return num2 * 2 ;
+      }
+}
+console.log(numbers(9))
+console.log(numbers(16))
+}
+
