a8ea10f371a56337e66d9fb55b6b2d2178dd4386
sucharita4867 <sucharitasardar7@gmail.com>
2025-05-26 16:20:34 +0530

    added sum and even numbers practice

--- js-function/even-only.js
+++ js-function/even-only.js
@@ -10,5 +10,20 @@ function evenNumbers (numbers){
       return even;
 }
 const nums = [2, 43, 34, 4, 56, 67];
-const even = evenNumbers(nums);
-console.log('even no :', even)
+// const even = evenNumbers(nums);
+// console.log('even no :', even)
+
+function sumOfEvenNumbers (numbers){
+      let sum = 0 ;
+      for(const number of numbers){
+            // console.log(number)
+            if(number % 2 === 0){
+                  console.log(number);
+                  sum = sum + number ;
+            }
+      }
+      return sum ;
+}
+const numb = [23, 42, 56, 52, 675, 678,];
+const sum = sumOfEvenNumbers (numb);
+console.log('sum of numbers is :', sum);
