9b32ac660c7e25cb62e6582e7c5d67e14e40fa91
sucharita4867 <sucharitasardar7@gmail.com>
2025-05-27 16:34:37 +0530

    practice function question ans

---
+++ ALL_PRACTICE.js/function-practice.js
@@ -0,0 +1,16 @@
+// task 1
+{
+      function number (num1 , num2){
+      const num = num1 + num2 ;
+      return num ;
+}
+const result = number(3 ,5);
+// console.log('return valu :' , result);
+// ----------------------
+function numbers ( numFirst , numSecond){
+      const number = numFirst + numSecond ;
+      return number ;
+}
+const output = numbers(20 ,30);
+// console.log('final look :' , output);
+}
