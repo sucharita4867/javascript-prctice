66ae149ab0e7ad43154562d61687368abd1c047d
sucharita4867 <sucharitasardar7@gmail.com>
2025-05-27 20:10:46 +0530

    practice function odd and even numbers

--- ALL_PRACTICE.js/function-practice.js
+++ ALL_PRACTICE.js/function-practice.js
@@ -14,3 +14,39 @@ function numbers ( numFirst , numSecond){
 const output = numbers(20 ,30);
 // console.log('final look :' , output);
 }
+// task 2
+{function name(fullname){
+      console.log('hello ,' + fullname + "!");
+}
+// name('suhana');
+}
+// task 3
+{function checkOddEven (num){
+      if(num % 2 === 0){
+            return 'even'
+      }
+      else{
+            return 'odd'
+      }
+      
+}
+// const result1 = checkOddEven (32);
+// console.log(result1);
+
+// const result2 = checkOddEven(90);
+// console.log(result2)
+// --------------------------------
+function oddAndEven (numbers){
+      if (numbers % 2 === 0){
+            return 'even';
+      }
+      else{
+            return 'odd';
+      }
+}
+// const total1 = oddAndEven(90);
+// console.log(total1);
+
+// const total2 = oddAndEven(40);
+// console.log(total2)
+}
