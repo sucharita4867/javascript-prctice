c005cb2f03cbdd05000ea8385bf2f55b9e8ef5b7
sucharita4867 <sucharitasardar7@gmail.com>
2025-06-12 16:47:05 +0530

    complet 5 of question in array methods

--- ALL_PRACTICE.js/array-methods-practice.js
+++ ALL_PRACTICE.js/array-methods-practice.js
@@ -1,4 +1,32 @@
-let fruits = ["mango", "banana"];
+// task 1
+{
+      let fruits = ["mango", "banana"];
 fruits.push('apple');
 fruits.shift();
-console.log(fruits)
+// console.log(fruits)
+}
+// task 2
+{
+      let devices = ["laptop", "tablet", "phone",];
+// devices.includes('phone');
+// devices.push('computer')
+// console.log(devices.includes('computer'))
+// console.log(devices)
+}
+// task 3
+{
+//       let nums = [10, 20, 30, 40];
+// nums.pop();
+// nums.push(99);
+// console.log(nums)
+}
+// task 4
+{
+      // let letters = ["a", "b", "c", "d", "e"];
+// let part = letters.slice(1,4);
+// console.log(part)
+}
+// task 5
+let names = ["Rina", "Mina", "Tina"];
+// names.join("-");
+console.log(names.join("-"));
