690bf6cc56049c8180e7d519b8d9bff926aca5df
sucharita4867 <sucharitasardar7@gmail.com>
2025-06-16 05:59:10 +0530

    start to day practice

--- ALL_PRACTICE.js/all.js
+++ ALL_PRACTICE.js/all.js
@@ -8,6 +8,38 @@ if(age > 18){
 }
 }
 // task 2
+{
+      let sum = 0;
 for(let i = 0 ;i <= 10 ;i++){
-      console.log(i)
+      // console.log(i)
+      sum += i;
+}
+// console.log('total sum number :', sum)
+}
+// task 3
+{
+      let fruits = ["apple", "banana"];
+const fruit = fruits.push('mango');
+// console.log(fruits);
+// console.log(fruit);
+}
+// task 4
+{
+      let colors = ["red", "green", "blue"];
+const color = colors.includes('green');
+// console.log(color) 
+
+if(color === true){
+      // console.log('Green Found!')
+}else{
+      // console.log('Green is Not Found')
+}
+}
+// task 5
+{
+      let i = 5 ;
+while (i > 0){
+      // console.log(i);
+      i-- ;
+}
 }
