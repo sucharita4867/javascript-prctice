e63ca2806ab17bd8d7a7b289f5c4b6f743754482
sucharita4867 <sucharitasardar7@gmail.com>
2025-06-16 23:20:35 +0530

    today practice question is complete

--- ALL_PRACTICE.js/all.js
+++ ALL_PRACTICE.js/all.js
@@ -70,9 +70,87 @@ const letter = letters.splice(1,3);
 }
 // -------------- 16 | 06 | 2025 ---------------
 // task 1
-let temp = 38;
+{
+      let temp = 38;
 if(temp > 37){
-      console.log('You may have fever');
+      // console.log('You may have fever');
 }else{
-      console.log('Normal')
+      // console.log('Normal')
+}
+}
+// task 2
+{
+      let subjects = ["Math", "English", "Biology"];
+const subject = subjects.includes();
+// console.log(subject)
+if(subject === true){
+      // console.log('Found Physics')
+}else{
+      // console.log('Not Found Physics')
+}
+}
+// task 3
+{
+      for(let i = 0 ; i <= 20 ; i++){
+      // console.log(i)
+      if(i % 2 === 0){
+            // console.log('even no :' ,i)
+      }
+}
+}
+// task 4
+{
+      let tasks = ["Wake up", "Brush"];
+const task = tasks.push('Exercise');
+// console.log(tasks.pop( ));
+// console.log(tasks)
+}
+// task 5
+{
+      let numbers = [12, 5, 7, 20];
+for(let i = 0 ; i < numbers.length ; i++){
+      if(numbers[i] > 10){
+            // console.log('Big Number :' , numbers[i])
+      }else{
+            // console.log('Small Number :', numbers[i])
+      }
+}
+}
+// task 6
+{
+      let i = 0;
+while(i <= 10){
+      // console.log(i);
+      i++ ;
+}
+}
+// task 7
+{
+      let words = ["We", "are", "learning", "JavaScript"];
+const word = words.slice(1,3);
+// console.log(word.join(' '));
+}
+// task 8
+{
+      let cities = ["Kolkata", "Delhi", "Mumbai", "Chennai"];
+const citiy = cities.splice(1,1)
+// console.log(cities)
+}
+// task 9
+{
+      let marks = 82;
+if(marks > 80){
+      // console.log('A+');
+}else if(marks > 60 && marks < 79){
+      // console.log('A');
+}else if(marks > 40 && marks < 59){
+      // console.log('B');
+}else{
+      // console.log('Fail');
+}
+}
+// task 10
+let names = ["Rita", "Sita", "Gita"];
+for(let i = 0 ; i < names.length ; i++){
+      console.log(names[i] ,'is present');
 }
