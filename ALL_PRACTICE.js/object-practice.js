29dfe1c4e31bf5eb3807672fa7b3adf677bacdcd
sucharita4867 <sucharitasardar7@gmail.com>
2025-06-21 11:31:14 +0530

    today practice object

---
+++ ALL_PRACTICE.js/object-practice.js
@@ -0,0 +1,15 @@
+let person = {
+      name : 'rajib' ,
+      age : 25 ,
+      city : 'kolkata'
+};
+{
+      // console.log(person.name)
+// console.log(person.age)
+// console.log(person.city)
+}
+{
+      // console.log(person['name'])
+// console.log(person['age'])
+// console.log(person['city'])
+}
