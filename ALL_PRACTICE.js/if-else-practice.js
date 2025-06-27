54c1e1deb6cbc096566604f653a99598f115bf43
sucharita4867 <sucharitasardar7@gmail.com>
2025-06-27 22:38:15 +0530

    added positive , nagetive and zero number

--- ALL_PRACTICE.js/if-else-practice.js
+++ ALL_PRACTICE.js/if-else-practice.js
@@ -537,11 +537,24 @@ if(i % 2 === 0 ){
 }
 }
 // task 2
-let a = 1450, b = 25, c = 165;
+{
+      let a = 1450, b = 25, c = 165;
 if(a > b && a > c){
-      console.log('big no is A')
+      // console.log('big no is A')
 }else if(b > a && b > c){
-      console.log('big no is B')
+      // console.log('big no is B')
 }else{
-      console.log('big no is C')
+      // console.log('big no is C')
+}
+}
+// task 3
+{
+      let number = 60;
+if(number > 0){
+      // console.log('positive no')
+}else if(number < 0){
+      // console.log('negative no')
+}else{
+      // console.log('zero no')
+}
 }
